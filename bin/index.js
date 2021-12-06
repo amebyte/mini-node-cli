174c5e115c1cf1134bce404afd7fd2a7c32d9ab7
unknown <416212316@qq.com>
2021-12-06 22:19:31 +0800

    chalk

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,7 @@
 
 import fs from 'fs'
 import { execaCommand } from 'execa'
+import chalk from 'chalk'
 import createIndexTemplate from './createIndexTemplate.js'
 import createPackageTemplate from './createPackageTemplate.js'
 import quesition from './question/index.js'
@@ -9,12 +10,16 @@ import { createConfig } from './config.js'
 const answer = await quesition()
 const inputConfig = createConfig(answer)
 // 1. 创建文件夹
+console.log(chalk.blue(`创建文件夹 -> ${inputConfig.packageName}`))
 fs.mkdirSync(getRootPath())
 // 2. 创建入口文件 -> index.js
+console.log(chalk.blue(`创建入口文件 -> index.js`))
 fs.writeFileSync(`${getRootPath()}/index.js`, createIndexTemplate(inputConfig))
 // 3. 创建package.json
+console.log(chalk.blue(`创建package.json`))
 fs.writeFileSync(`${getRootPath()}/package.json`, createPackageTemplate(inputConfig))
 // 4. 安装依赖
+console.log(chalk.blue(`安装依赖`))
 execaCommand('yarn', {
     cwd: getRootPath(),
     // stdio: ['pipe', 'inherit', 'ipc']
